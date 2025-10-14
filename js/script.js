4bf0c505bc2c97aaf0754eaa4260ede0ac537d4f
karis-cod <chukwuebukaogu3@gmail.com>
2025-10-14 01:08:51 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,14 +1,11 @@
- const inpEle = document.querySelector('#inp');
+const inpEle = document.querySelector('#inp');
 const todoInpEle = document.querySelector('.todo-ul');
 const addTodoEle = document.querySelector('.addtodo');
 const formTodoEle = document.querySelector('.todo-form');
 const dateTodoEle = document.querySelector('#date');
 const liTodoEle = document.querySelector('.todo-li');
 
-todolist = [{
-  name: 'suger',
-  duedate: '2-22-222 2'
-}];
+todolist = [];
 
 renderTodo()
 
@@ -17,14 +14,19 @@ function renderTodo() {
   for (let i = 0; i < todolist.length; i++) {
     const todoObj = todolist[i];
     const {name, duedate} = todoObj
-    const html = `<li class="todo-li">${name} <span class="lispn">${duedate}</span><button onclick="
-    todolist.splice(${i},1); renderTodo();"
-    class="del">Delete</button></li>`;
+    const html = `
+      <li class="todo-li">${name}<span class="rap"><span class="lispn">${duedate}</span>
+      <button onclick="
+      todolist.splice(${i},1); renderTodo();"
+      class="del">Delete</button></span></li>
+    `;
+
     todolistHTML += html
   }
-todoInpEle.innerHTML = todolistHTML;
-};
 
+todoInpEle.innerHTML = todolistHTML;
+
+};
 
 addTodoEle.addEventListener('click', () =>{
     const name = inpEle.value;
@@ -33,6 +35,7 @@ addTodoEle.addEventListener('click', () =>{
     inpEle.value = ''
     renderTodo()
 });
+
  formTodoEle.addEventListener('submit', (event) => {
   event.preventDefault()
-})
+});
