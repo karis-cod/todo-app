735efd72f05adbce2d199c7a38aef723f4b900ea
karis-cod <chukwuebukaogu3@gmail.com>
2025-10-22 00:06:23 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ const todoInpEle = document.querySelector('.todo-ul');
 const addTodoEle = document.querySelector('.addtodo');
 const formTodoEle = document.querySelector('.todo-form');
 const dateTodoEle = document.querySelector('#date');
+const timeTodoEle = document.querySelector('#time');
 const liTodoEle = document.querySelector('.todo-li');
 
-todolist = [];
+
+todolist = JSON.parse(localStorage.getItem("todolist")) || [];
 
 renderTodo()
 
@@ -13,11 +15,11 @@ function renderTodo() {
   let todolistHTML = '';
   for (let i = 0; i < todolist.length; i++) {
     const todoObj = todolist[i];
-    const {name, duedate} = todoObj
+    const {name, duedate, time} = todoObj
     const html = `
-      <li class="todo-li">${name}<span class="rap"><span class="lispn">${duedate}</span>
+      <li class="todo-li"> ${name}<span class="rap"><span class="datt">${duedate} </span> ${time}
       <button onclick="
-      todolist.splice(${i},1); renderTodo();"
+      todolist.splice(${i},1); renderTodo(); saveTodo();"
       class="del">Delete</button></span></li>
     `;
 
@@ -27,13 +29,30 @@ function renderTodo() {
 todoInpEle.innerHTML = todolistHTML;
 
 };
+function saveTodo() {
+  localStorage.setItem("todolist", JSON.stringify(todolist));
+}
 
 addTodoEle.addEventListener('click', () =>{
+  if (inpEle.value && timeTodoEle.value && dateTodoEle.value) {
     const name = inpEle.value;
     const duedate = dateTodoEle.value;
-    todolist.push({name,duedate});
-    inpEle.value = ''
-    renderTodo()
+    const time = timeTodoEle.value;
+    todolist.push({name,duedate,time});
+    inpEle.value = '';
+    saveTodo();
+    renderTodo();    
+  }
+  else{
+    const name = 'description ERROR!!!';
+    const duedate = '';
+    const time = '';
+    todolist.push({name,duedate,time});
+    inpEle.value = '';
+    saveTodo();
+    renderTodo(); 
+  }
+
 });
 
  formTodoEle.addEventListener('submit', (event) => {
